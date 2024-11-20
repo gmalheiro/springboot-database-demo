ca5131a25b4e5fb92c5cefbe0010dc5e81695317
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-11-20 11:31:48 -0300

    feat: add crud methods

--- spring-database-app/src/main/java/com/spring/database/app/spring_database_app/data/jdbc/CarJdbcDao.java
+++ spring-database-app/src/main/java/com/spring/database/app/spring_database_app/data/jdbc/CarJdbcDao.java
@@ -11,13 +11,30 @@ import com.spring.database.app.spring_database_app.entity.Car;
 
 @Repository
 public class CarJdbcDao {
-	
+
 	@Autowired
 	JdbcTemplate jdbcTemplate;
-	
-	public List<Car> findAll () {
+
+	public List<Car> findAll() {
 		return jdbcTemplate.query("select * from car", new CarRowMapper());
 	}
+
+	public Car findCarById(int id) {
+		return jdbcTemplate.queryForObject("select * from car where id =?", new CarRowMapper(), id);
+	}
+
+	public int insertCar(Car car) {
+		return jdbcTemplate.update("insert into car (id,model,brand) values (?,?,?)",
+				car.getId(),car.getModel(),car.getBrand());
+	}
 	
+	public int updateCar(Car car) {
+		return jdbcTemplate.update("update car set model = ?, brand = ? where id = ?",
+				car.getModel(),car.getBrand(),car.getId());
+	}
 	
+	public int deleteCar(int id) {
+		return jdbcTemplate.update("delete from car where id =? ",id);
+	}
+
 }
